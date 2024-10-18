2898446c2dc32c1600b2eb55fd41ca0f4e877e8c
Boranroni <84103778+boranroni@users.noreply.github.com>
2024-10-18 17:39:46 +0300

    Update userconfig.js
    Deleted extra tabs

--- userconfig.js
+++ userconfig.js
@@ -1,200 +1,99 @@
 const CONFIG = new Config({
-  crypto: {
-    coin: 'ETH',
-    currency: 'USD',
-    refreshIn: 10
-  },
-  overrideStorage: true, // override localStorage with fixed userconfig values
-  temperature: {
-    location: 'Istanbul, Turkey',
-    scale: 'C'
-  },
-  clock: {
-    format: 'h:i p',
-    iconColor: '#ff7b95'
-  },
-  search: {
-    engines: {
-      g: ['https://google.com/search?q=', 'Google'],
-      i: ['https://ixquick.com/do/search?q=', 'Ixquick'],
-      d: ['https://duckduckgo.com/html?q=', 'DuckDuckGo'],
-      y: ['https://youtube.com/results?search_query=', 'Youtube'],
-      w: ['https://en.wikipedia.org/w/index.php?search=', 'Wikipedia']
-    }
-  },
-  keybindings: {
-    "t": 'todo-list',
-    "s": 'search-bar'
-  },
-  disabled: ['crypto'],
-  openLastVisitedTab: false,
-  tabs: [
-    {
-      name: 'School',
-      background_url: 'src/img/banners/bg-1.gif',
-      categories: [{
-        name: 'Sites',
-        links: [
-          {
-            url: 'https://obs.yeditepe.edu.tr/',
-            name: '/obs/'
-          },
-          {
-            name: '/yulr/',
-            url: 'https://yulearn.yeditepe.edu.tr/login/index.php',
-            icon: 'mountain',
-            icon_color: '#4957F2'
-          },
-          {
-            name: '/lit/',
-            url: 'https://calendar.google.com/',
-            icon: 'calendar-clock'
-          },
-          {
-            name: '/imgSearch/',
-            url: 'https://images.google.com/',
-            icon: 'camera'
-          }
-        ]
-      },
+    crypto: {
+      coin: 'ETH',
+      currency: 'USD',
+      refreshIn: 10
+    },
+    overrideStorage: true, // override localStorage with fixed userconfig values
+    temperature: {
+      location: 'Istanbul, Turkey',
+      scale: 'C'
+    },
+    clock: {
+      format: 'h:i p',
+      iconColor: '#ff7b95'
+    },
+    search: {
+      engines: {
+        g: ['https://google.com/search?q=', 'Google'],
+        i: ['https://ixquick.com/do/search?q=', 'Ixquick'],
+        d: ['https://duckduckgo.com/html?q=', 'DuckDuckGo'],
+        y: ['https://youtube.com/results?search_query=', 'Youtube'],
+        w: ['https://en.wikipedia.org/w/index.php?search=', 'Wikipedia']
+      }
+    },
+    keybindings: {
+      "t": 'todo-list',
+      "s": 'search-bar'
+    },
+    disabled: ['crypto'],
+    openLastVisitedTab: false,
+    tabs: [
       {
-        name: 'Comfy',
-        links: [
-          {
-            name: '/lounge/',
-            url: 'https://www.youtube.com/@LofiGirl/streams'
-          },
-        ]
-      },
-      {
-        name: 'Technology',
-        links: [
-          {
-            name: '/mDoc/',
-            url: 'https://www.mathworks.com/help/matlab/',
-          },
-          {
-            name: '/Docs/',
-            url: 'https://docs.google.com/'
-          },
-          {
-            name: '/λ/',
-            url: 'https://www.gmail.com',
-            icon: 'mail'
-          },
-          {
-            name: '/calc/',
-            url: 'https://www.symbolab.com/'
-          },
-          {
-            name: '/prog/',
-            url: 'https://www.programiz.com/python-programming/online-compiler/',
-            icon: 'code'
-          }
+        name: 'School',
+        background_url: 'src/img/banners/bg-1.gif',
+        categories: [{
+          name: 'Sites',
+          links: [
+            {
+              url: 'https://obs.yeditepe.edu.tr/',
+              name: '/obs/'
+            },
+            {
+              name: '/yulr/',
+              url: 'https://yulearn.yeditepe.edu.tr/login/index.php',
+              icon: 'mountain',
+              icon_color: '#4957F2'
+            },
+            {
+              name: '/lit/',
+              url: 'https://calendar.google.com/',
+              icon: 'calendar-clock'
+            },
+            {
+              name: '/imgSearch/',
+              url: 'https://images.google.com/',
+              icon: 'camera'
+            }
+          ]
+        },
+        {
+          name: 'Comfy',
+          links: [
+            {
+              name: '/lounge/',
+              url: 'https://www.youtube.com/@LofiGirl/streams'
+            },
+          ]
+        },
+        {
+          name: 'Technology',
+          links: [
+            {
+              name: '/mDoc/',
+              url: 'https://www.mathworks.com/help/matlab/',
+            },
+            {
+              name: '/Docs/',
+              url: 'https://docs.google.com/'
+            },
+            {
+              name: '/λ/',
+              url: 'https://www.gmail.com',
+              icon: 'mail'
+            },
+            {
+              name: '/calc/',
+              url: 'https://www.symbolab.com/'
+            },
+            {
+              name: '/prog/',
+              url: 'https://www.programiz.com/python-programming/online-compiler/',
+              icon: 'code'
+            }
+          ]
+        }
         ]
       }
-      ]
-    },
-    {
-      name: 'music',
-      background_url: 'src/img/banners/bg-2.gif',
-      categories: [
-        {
-          name: 'music services',
-          links: [
-            {
-              url: 'https://soundcloud.com/',
-              icon: 'brand-soundcloud',
-              icon_color: '#c57750'
-            },
-            {
-              url: 'https://youtu.be/',
-              icon: 'brand-youtube',
-              icon_color: '#996767'
-            },
-            {
-              url: 'https://r-a-d.io/',
-              icon: 'radio'
-            },
-          ]
-        },
-        {
-          name: 'music boards',
-          links: [
-            {
-              name: '/music/',
-              url: 'https://lainchan.org/music/',
-              icon: 'disc'
-            },
-            {
-              name: '/mu/',
-              url: 'https://boards.4channel.org/mu/',
-            }
-          ]
-        }
-      ]
-    },
-    {
-      name: 'tech',
-      background_url: 'src/img/banners/bg-3.gif',
-      categories: [
-        {
-          name: 'subreddits',
-          links: [
-            {
-              name: 'r/startpages/',
-              url: 'https://www.reddit.com/r/startpages/'
-            },
-            {
-              name: 'r/unixporn',
-              url: 'https://www.reddit.com/r/unixporn/'
-            },
-            {
-              name: 'r/mechkbds/',
-              url: 'https://www.reddit.com/r/MechanicalKeyboards/',
-              icon: 'keyboard',
-              icon_color: '#a57685'
-            },
-            {
-              name: 'r/programming',
-              url: 'https://www.reddit.com/r/programming/'
-            }
-          ]
-        },
-        {
-          name: 'blogs',
-          links: [
-            {
-              name: 'fasterthanli',
-              url: 'https://fasterthanli.me/articles',
-              icon: 'anchor'
-            },
-            {
-              name: 'dev.to',
-              url: 'https://dev.to'
-            },
-            {
-              name: 'mataroa.blog',
-              url: 'https://collection.mataroa.blog',
-            }
-          ]
-        },
-        {
-          name: 'misc',
-          links: [
-            {
-              name: 'post office',
-              url: 'http://afternoon.dynu.com/letterbox.html',
-              icon: 'mailbox'
-            },
-            {
-              name: 'rust docs',
-              url: 'https://doc.rust-lang.org/book/',
-              icon: 'notebook',
-              icon_color: '#977a3a'
-            }
-          ]
-        }
-      ]
-    }]
-});
+    ]
+  });
